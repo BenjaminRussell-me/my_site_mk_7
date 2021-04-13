399f992b47861e95948873293aa89337ed47ca50
Benjamin <contact@benjaminrussell.me>
2021-04-12 22:58:59 -0500

    removed unused component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
   {{ dataState }}
   <router-view />
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 import { dataStore } from "./store/data";
 export default defineComponent({
   name: "App",
-  components: {
-    HelloWorld,
-  },
   setup() {
     onMounted(() => {
       dataStore.setData("Projects");
